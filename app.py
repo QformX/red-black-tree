62e9761b4aa1a6e6793ffe38bcdb715cdfb1949d
QForm <mr.bogdan2003@gmail.com>
2024-04-23 19:51:33 +0600

    feat: add gradio UI

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 from tree import RedBlackTree
 from visualize import draw_tree
+import gradio as gr
+import matplotlib.pyplot as plt
+import numpy as np
+
+empty_tree = RedBlackTree()
+
+def insert_nodes(args):
+    for item in args.split(','):
+        empty_tree.insert(item)
+    return draw_tree(empty_tree.root, 0, 0, 50)
+
+def delete_nodes(args):
+    for item in args.split(','):
+        empty_tree.delete_node(item)
+    return draw_tree(empty_tree.root, 0, 0, 50)
+    
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            with gr.Column():
+                args = gr.Textbox(label="Объекты заполняются через запятую")
+            with gr.Row():
+                with gr.Column():
+                    btn1 = gr.Button("Добавить") 
+                with gr.Column():
+                    btn2 = gr.Button("Удалить")
+        with gr.Column():
+            out = gr.Plot()
+    btn1.click(insert_nodes, inputs=args, outputs=out)
+    btn2.click(delete_nodes, inputs=args, outputs=out)
 
 if __name__ == "__main__":
-    bst = RedBlackTree()
- 
-    bst.insert(70)
-    bst.insert(60)
-    bst.insert(85)
-    bst.insert(80)
-    bst.insert(95)
-    bst.insert(65)
-    
-    dx = 50  # горизонтальное расстояние между узлами
-    dy = 1  # вертикальное расстояние между уровнями
-
-    draw_tree(bst.root, 0, 0, dx, "pics/before_remove_graph.jpg")
-    print("\nAfter deleting an element")
-    bst.delete_node(60)
-    draw_tree(bst.root, 0, 0, dx, "pics/after_remove_graph.jpg")
+    demo.launch()
--- visualize.py
+++ visualize.py
@@ -21,10 +21,9 @@ def render_red_black_tree(node, x, y, dx):
         render_red_black_tree(node.right, x + dx, y - dy, dx/2)
 
 
-def draw_tree(node, x, y, dx, pic_name):
-    plt.figure(figsize=(10, 10))
+def draw_tree(node, x, y, dx):
+    fig = plt.figure(figsize=(10, 10))
     plt.axis('off')
     render_red_black_tree(node, x, y, dx)
-    plt.savefig(pic_name)
-    plt.show()
+    return fig
 
